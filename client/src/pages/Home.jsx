745a89f19e6c269683b77d3b6d28cdf82af465b0
moha-simo <mohamesd@gmail.com>
2024-10-02 16:17:14 +0100

    fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -52,10 +52,10 @@ export default function Home() {
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
           Find your next <span className="text-slate-500">perfect</span>
           <br />
-          place with ease
+          place
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
+          Real Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
